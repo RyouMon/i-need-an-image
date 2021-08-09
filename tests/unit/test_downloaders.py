254a4a65f5c21db59553d267d975e7d3cec01f06
Wen <wenslife@outlook.com>
2021-08-09 23:28:33 +0800

    test(downloader): test BingImage.search_request

--- tests/unit/test_downloaders.py
+++ tests/unit/test_downloaders.py
@@ -20,12 +20,22 @@ class BingImageTest(TestCase):
             mode='RBG', size=(1920, 1080), data=mock_download_image.return_value
         )
 
-    @patch('need_an_image.downloader.BingImage.request')
+    @patch('need_an_image.downloader.BingImage.search_request')
     @patch('need_an_image.downloader.BingImage.parse_source')
-    def test_get_image_info(self, mock_parse_source, mock_request):
+    def test_get_image_info(self, mock_parse_source, mock_search_request):
         bing = downloader.BingImage()
 
         bing.get_image_info('Cat')
 
-        mock_request.assert_called_once_with(keyword='Cat')
-        mock_parse_source.assert_called_once_with(mock_request.return_value)
+        mock_search_request.assert_called_once_with(keyword='Cat')
+        mock_parse_source.assert_called_once_with(mock_search_request.return_value)
+
+    @patch('need_an_image.downloader.requests')
+    @patch('need_an_image.downloader.BingImage.get_search_params')
+    def test_search_request(self, mock_get_search_params, mock_requests):
+        bing = downloader.BingImage()
+
+        bing.search_request('Cat')
+
+        mock_get_search_params.assert_called_once_with(keyword='Cat')
+        mock_requests.get.assert_called_once_with(bing.url, params=mock_get_search_params.return_value)
