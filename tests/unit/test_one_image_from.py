53571c4dc63dc681a860b302a13d9934b4e3c9b4
Wen <wenslife@outlook.com>
2021-08-09 22:31:54 +0800

    test: new test for from_bing() function

--- tests/unit/test_one_image_from.py
+++ tests/unit/test_one_image_from.py
@@ -3,11 +3,23 @@ from unittest.mock import patch
 import need_an_image
 
 
-class OneImageFromBingTest(TestCase):
+class FromBingTest(TestCase):
 
     @patch('need_an_image.bing')
-    def test_call_bing_get_an_image(self, mock_bing):
-        filename = need_an_image.from_bing(keyword='猫')
+    @patch('need_an_image.save_image')
+    def test_from_bing_return_filename_if_save_is_True(self, mock_save_image, mock_bing):
+        filename = need_an_image.from_bing(keyword='Cat', save=True)
+        image = mock_bing.get_an_image.return_value
 
-        mock_bing.get_an_image.assert_called_once_with(keyword='猫')
-        self.assertEqual(filename, mock_bing.get_an_image.return_value)
+        mock_bing.get_an_image.assert_called_once_with(keyword='Cat')
+        mock_save_image.assert_called_once_with(image)
+        self.assertEqual(filename, mock_save_image.return_value)
+
+    @patch('need_an_image.bing')
+    @patch('need_an_image.save_image')
+    def test_from_bing_return_Image_if_save_is_False(self, mock_save_image, mock_bing):
+        image = need_an_image.from_bing(keyword='Cat', save=False)
+
+        mock_bing.get_an_image.assert_called_once_with(keyword='Cat')
+        mock_save_image.assert_not_called()
+        self.assertEqual(image, mock_bing.get_an_image.return_value)
