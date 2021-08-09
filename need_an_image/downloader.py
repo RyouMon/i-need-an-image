eefc6704c558eae420cdc549c5e76f9cc127b9e1
Wen <wenslife@outlook.com>
2021-08-09 23:30:03 +0800

    feat(downloader): finish search_request(), define url attribute, placeholder get_search_params()

--- need_an_image/downloader.py
+++ need_an_image/downloader.py
@@ -1,10 +1,13 @@
+import requests
 from PIL import Image
 
 
 class BingImage:
 
+    url = 'https://cn.bing.com/images/search'
+
     def get_image_info(self, keyword):
-        response = self.request(keyword=keyword)
+        response = self.search_request(keyword=keyword)
         return self.parse_source(response)
 
     def get_an_image(self, keyword):
@@ -12,11 +15,18 @@ class BingImage:
         image_content = self.download_image(source_url)
         return Image.frombytes(size=size, mode=mode, data=image_content)
 
+    def get_search_params(self, keyword):
+        return {'q': keyword}
+
     def download_image(self, source_url):
         pass
 
     def parse_source(self, response):
         return 'RGB', (1, 1), 'https://example.com/image.png'
 
-    def request(self, keyword):
-        pass
+    def search_request(self, keyword):
+        """
+        Send an image search search_request
+        """
+        payload = self.get_search_params(keyword=keyword)
+        return requests.get(self.url, params=payload)
