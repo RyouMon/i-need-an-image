d9aaf7913dced2b59d5077b65b40c520074a00db
Wen <wenslife@outlook.com>
2021-08-09 22:53:04 +0800

    feat(downloader): finish BingImage.get_an_image, placeholder get_image_info and download_image.

---
+++ need_an_image/downloader.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+
+class BingImage:
+
+    def get_image_info(self, keyword):
+        response = self.request(keyword=keyword)
+        return self.parse_source(response)
+
+    def get_an_image(self, keyword):
+        mode, size, source_url = self.get_image_info(keyword)
+        image_content = self.download_image(source_url)
+        return Image.frombytes(size=size, mode=mode, data=image_content)
+
+    def download_image(self, source_url):
+        pass
