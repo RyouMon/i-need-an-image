a8e43ecb6b3affc86665d857a45e44cbc701ee72
Wen <wenslife@outlook.com>
2021-08-09 22:32:54 +0800

    feat: finish from_bing(), placeholder save_image and BingImage

--- need_an_image/__init__.py
+++ need_an_image/__init__.py
@@ -1,5 +1,32 @@
 import requests
+from PIL import Image
 
 
-def from_bing(keyword):
-    return None
+class BingImage:
+
+    def get_an_image(self, keyword):
+        response = self.search_request(keyword=keyword)
+        source_url = self.parse_source(response)
+        image_content = self.download_content(source_url)
+        return Image.frombytes(image_content)
+
+
+bing = BingImage()
+
+
+def save_image(image):
+    pass
+
+
+def from_bing(keyword, save=True):
+    """
+    Get an image from Bing image.
+    if save = True , image with save in disk and return a filename,
+    if save = False, return an PIL.Image object.
+    """
+    image = bing.get_an_image(keyword=keyword)
+
+    if save:
+        return save_image(image)
+
+    return image
