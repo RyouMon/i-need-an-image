beeb348f397b3371dd883141cf35efc31fc4c18c
Wen <wenslife@outlook.com>
2021-08-10 21:44:29 +0800

    test(utils): add test for save_image()

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+import need_an_image
+
+
+class SaveImageTest(TestCase):
+
+    @patch('need_an_image.uuid4')
+    def test_save_Image_to_disk(self, mock_uuid4):
+        mock_image = MagicMock()
+        mock_uuid4.return_value.hex = 'image'
+        filename = need_an_image.save_image(mock_image)
+
+        mock_image.save.assert_called_once_with('image.jpg', 'JPEG')
+        self.assertEqual(filename, 'image.jpg')
