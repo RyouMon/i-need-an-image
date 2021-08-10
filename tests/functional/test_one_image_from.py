77c7d3ce6e5cacac4c6bc261d3c5df341605a8ee
Wen <wenslife@outlook.com>
2021-08-10 21:44:06 +0800

    test(functional): fix test

--- tests/functional/test_one_image_from.py
+++ tests/functional/test_one_image_from.py
@@ -7,14 +7,16 @@ import need_an_image
 class OneImageFromBingTest(TestCase):
 
     def test_save_an_image_from_bing(self):
-        file_nums = len(os.listdir('tests'))
+        file_nums = len(os.listdir())
         filename = need_an_image.from_bing(keyword='猫')
 
-        self.assertEqual(len(os.listdir('tests')), file_nums + 1)
-        Image.open(filename)  # should not raise
+        self.assertEqual(len(os.listdir()), file_nums + 1)
+        with Image.open(filename):
+            pass  # should not raise
+
+        os.remove(filename)
 
     def test_create_an_image_from_bing(self):
         image = need_an_image.from_bing(keyword='猫', save=False)
 
-        image.show()
         self.assertIsInstance(image, Image.Image)
