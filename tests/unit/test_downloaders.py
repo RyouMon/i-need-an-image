f4a62bd1dd7f7f5869eabfd9b5779c182b286b24
Wen <wenslife@outlook.com>
2021-08-10 22:40:08 +0800

    test(downloader): update test_get_an_image

--- tests/unit/test_downloaders.py
+++ tests/unit/test_downloaders.py
@@ -5,27 +5,27 @@ from need_an_image import downloader
 
 class BingImageTest(TestCase):
 
+    @patch('need_an_image.downloader.BytesIO')
     @patch('need_an_image.downloader.Image')
     @patch('need_an_image.downloader.BingImage.download_image')
-    @patch('need_an_image.downloader.BingImage.get_image_info')
-    def test_get_an_image(self, mock_get_image_info, mock_download_image, mock_image_class):
+    @patch('need_an_image.downloader.BingImage.get_image_source_url')
+    def test_get_an_image(self, mock_get_image_source_url, mock_download_image, mock_image_class, mock_BytesIO):
         bing = downloader.BingImage()
-        mock_get_image_info.return_value = ('RBG', (1920, 1080), 'https://example.com/exmple.jpg')
+        mock_get_image_source_url.return_value = 'https://example.com/exmple.jpg'
 
         bing.get_an_image('Cat')
 
-        mock_get_image_info.assert_called_once_with('Cat')
+        mock_get_image_source_url.assert_called_once_with('Cat')
         mock_download_image.assert_called_once_with('https://example.com/exmple.jpg')
-        mock_image_class.frombytes.assert_called_once_with(
-            mode='RBG', size=(1920, 1080), data=mock_download_image.return_value
-        )
+        mock_BytesIO.assert_called_once_with(mock_download_image.return_value)
+        mock_image_class.open.assert_called_once_with(mock_BytesIO.return_value)
 
     @patch('need_an_image.downloader.BingImage.search_request')
     @patch('need_an_image.downloader.BingImage.parse_source')
     def test_get_image_info(self, mock_parse_source, mock_search_request):
         bing = downloader.BingImage()
 
-        bing.get_image_info('Cat')
+        bing.get_image_source_url('Cat')
 
         mock_search_request.assert_called_once_with(keyword='Cat')
         mock_parse_source.assert_called_once_with(mock_search_request.return_value)
