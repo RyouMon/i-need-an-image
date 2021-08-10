56b0d3fa3c557ffe490556c976646d6578671989
Wen <wenslife@outlook.com>
2021-08-10 20:55:18 +0800

    test(functional): test create an Image instance from bing

--- tests/functional/test_one_image_from.py
+++ tests/functional/test_one_image_from.py
@@ -6,9 +6,15 @@ import need_an_image
 
 class OneImageFromBingTest(TestCase):
 
-    def test_download_an_image_from_bing(self):
+    def test_save_an_image_from_bing(self):
         file_nums = len(os.listdir('tests'))
         filename = need_an_image.from_bing(keyword='猫')
 
         self.assertEqual(len(os.listdir('tests')), file_nums + 1)
         Image.open(filename)  # should not raise
+
+    def test_create_an_image_from_bing(self):
+        image = need_an_image.from_bing(keyword='猫', save=False)
+
+        image.show()
+        self.assertIsInstance(image, Image.Image)
