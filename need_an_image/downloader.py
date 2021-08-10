eac46dceab72a913e10b79125e6d6a1bb6def436
Wen <wenslife@outlook.com>
2021-08-10 22:45:45 +0800

    feat(downloader): finish BingImage.parse_source

--- need_an_image/downloader.py
+++ need_an_image/downloader.py
@@ -1,6 +1,10 @@
+import json
+import random
 from io import BytesIO
+
 import requests
 from PIL import Image
+from bs4 import BeautifulSoup
 
 
 class BingImage:
@@ -24,7 +28,15 @@ class BingImage:
         return response.content
 
     def parse_source(self, response):
-        return 'https://example.com/image.png'
+        soup = BeautifulSoup(response.text, features='lxml')
+
+        image_component = soup.find(id='mmComponent_images_1')
+        images = image_component.find_all('a', class_='iusc')
+        image = random.choice(images)
+        metadata = json.loads(image['m'])
+        source_url = metadata['murl']
+
+        return source_url
 
     def search_request(self, keyword):
         """
