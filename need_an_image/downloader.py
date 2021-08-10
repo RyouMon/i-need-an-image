714eb6f0e090041603b8479f5c49d76ddfaf875d
Wen <wenslife@outlook.com>
2021-08-10 22:43:13 +0800

    refactor(downloader): rename get_image_info to get_image_source_url. using Image.open instead of Image.frombytes

--- need_an_image/downloader.py
+++ need_an_image/downloader.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import requests
 from PIL import Image
 
@@ -6,14 +7,14 @@ class BingImage:
 
     url = 'https://cn.bing.com/images/search'
 
-    def get_image_info(self, keyword):
+    def get_image_source_url(self, keyword):
         response = self.search_request(keyword=keyword)
         return self.parse_source(response)
 
     def get_an_image(self, keyword):
-        mode, size, source_url = self.get_image_info(keyword)
+        source_url = self.get_image_source_url(keyword)
         image_content = self.download_image(source_url)
-        return Image.frombytes(size=size, mode=mode, data=image_content)
+        return Image.open(BytesIO(image_content))
 
     def get_search_params(self, keyword):
         return {'q': keyword}
@@ -23,7 +24,7 @@ class BingImage:
         return response.content
 
     def parse_source(self, response):
-        return 'RGB', (1, 1), 'https://example.com/image.png'
+        return 'https://example.com/image.png'
 
     def search_request(self, keyword):
         """
