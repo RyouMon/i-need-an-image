161f92ba890a3ef6b1746314f91a278f6bd739ff
Wen <wenslife@outlook.com>
2021-08-12 20:32:02 +0800

    refactor(downloader): refactor BingImage

--- need_an_image/downloader.py
+++ need_an_image/downloader.py
@@ -11,29 +11,7 @@ class BingImage:
 
     url = 'https://cn.bing.com/images/search'
 
-    def get_image_source_url(self, keyword):
-        response = self.search_request(keyword=keyword)
-        return self.parse_source(response)
-
-    def get_an_image(self, keyword):
-        """
-        return an Image matching keyword from web
-        """
-        source_url = self.get_image_source_url(keyword)
-        image_content = self.download_image(source_url)
-        return Image.open(BytesIO(image_content))
-
-    def get_search_params(self, keyword):
-        return {'q': keyword}
-
-    def download_image(self, source_url):
-        """
-        request picture, return binary data
-        """
-        response = requests.get(source_url)
-        return response.content
-
-    def parse_source(self, response):
+    def get_image_source_url(self, response):
         soup = BeautifulSoup(response.text, features='lxml')
 
         image_component = soup.find(id='mmComponent_images_1')
@@ -44,9 +22,25 @@ class BingImage:
 
         return source_url
 
-    def search_request(self, keyword):
+    def get_an_image(self, keyword):
+        """
+        return an Image matching keyword from web
+        """
+        response = self.download_search_page(keyword)
+        source_url = self.get_image_source_url(response)
+        image_content = self.download_image(source_url)
+        return Image.open(BytesIO(image_content))
+
+    def download_image(self, source_url):
+        """
+        request picture, return binary data
+        """
+        response = requests.get(source_url)
+        return response.content
+
+    def download_search_page(self, keyword):
         """
         Send an image search search_request
         """
-        payload = self.get_search_params(keyword=keyword)
+        payload = {'q': keyword}
         return requests.get(self.url, params=payload)
