e18a210557ae0cd57ea2614ab0003b2802ac8a66
Wen <wenslife@outlook.com>
2021-08-12 22:27:50 +0800

    feat(decorator): add decorator for request retry

---
+++ need_an_image/decorators.py
@@ -0,0 +1,21 @@
+import functools
+from requests.exceptions import RequestException
+
+
+def retry_request(max_retry=3):
+    """decorator for request retry"""
+
+    def concrete_decorator(function):
+
+        @functools.wraps(function)
+        def wrapped(*args, **kwargs):
+            retry = max_retry
+            while retry:
+                try:
+                    return function(*args, **kwargs)
+                except RequestException as e:
+                    retry -= 1
+            return function(*args, **kwargs)
+        return wrapped
+
+    return concrete_decorator
