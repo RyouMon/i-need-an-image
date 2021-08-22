59b1fd6d12968de01422943919585de5f50650d7
Wen <wenslife@outlook.com>
2021-08-22 13:00:20 +0800

    test: move save_image() to utils/store.py

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,26 +1,26 @@
 import os
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
-import need_an_image
+import utils.store
 
 
 class SaveImageTest(TestCase):
 
-    @patch('need_an_image.uuid4')
+    @patch('utils.store.uuid4')
     def test_save_Image_to_current_path(self, mock_uuid4):
         mock_image = MagicMock()
         mock_uuid4.return_value.hex = 'image'
-        filename = need_an_image.save_image(mock_image)
+        filename = utils.store.save_image(mock_image)
 
         save_to_path = os.path.join('.', 'image.jpg')
         mock_image.save.assert_called_once_with(save_to_path)
         self.assertEqual(filename, save_to_path)
 
-    @patch('need_an_image.uuid4')
+    @patch('utils.store.uuid4')
     def test_save_Image_to_other_path(self, mock_uuid4):
         mock_image = MagicMock()
         mock_uuid4.return_value.hex = 'image'
-        filename = need_an_image.save_image(mock_image, save_to='/tmp/')
+        filename = utils.store.save_image(mock_image, save_to='/tmp/')
 
         save_to_path = os.path.join('/tmp/', 'image.jpg')
         mock_image.save.assert_called_once_with(save_to_path)
